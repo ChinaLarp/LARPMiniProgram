3bf750fc38d41d33acef0343c3d1a71474a93b18
Nameedwang09 <edwang09@gmail.com>
2018-01-28 13:42:37 -0500

    ipdate

--- app.js
+++ app.js
@@ -18,42 +18,7 @@ App({
           success:function(res){
             console.log("unionid:" +res.data)
             that.globalData.unionid = res.data
-            wx.request({
-              method:"POST",
-              data:{
-                openid:res.data
-              },
-              url: 'http://localhost:8080/unifiedorder',
-              success: function (res) {
-                console.log(res.data.xml.prepay_id)
-                wx.requestPayment(
-                  {
-                    'timeStamp': '1490840662',
-                    'nonceStr': '123',
-                    'package': 'prepay_id='+res.data.xml.prepay_id,
-                    'signType': 'MD5',
-                    'paySign': md5.hexMD5("appId=wxf0487d45228f02d3&nonceStr=123&package=prepay_id=" + res.data.xml.prepay_id +"&signType=MD5&timeStamp=1490840662&key=6e11af317a7a85a14b3387d5c6c71d3a").toUpperCase(),
-                    'success': function (res) {console.log("success") },
-                    'fail': function (res) { console.log("failed")},
-                    'complete': function (res) { console.log("complete" )}
-                  })
-              }
-            })
           },
-          failed: function(res){
-            console.log("unionid failed to recieve, trying again")
-            wx.request({
-              url: 'https://chinabackend.bestlarp.com/unionid?appid=' + appid + '&secret=' + secret + '&js_code=' + res.code + '&grant_type=authorization_code',
-              success: function (res) {
-                console.log("unionid:" + res.data)
-                that.globalData.unionid = res.data
-              },
-              failed: function (res) {
-                console.log("unionid failed to recieve.")
-
-              }
-            })
-          }
         })
       }
     })
--- pages/game/game.js
+++ pages/game/game.js
@@ -3,7 +3,29 @@ Page({
     background: ['green', 'red', 'yellow'],
     gameinfo: null,
   },
-
+  purchase:function(){
+  wx.request({
+    method: "POST",
+    data: {
+      openid: res.data
+    },
+    url: 'http://localhost:8089/unifiedorder',
+    success: function (res) {
+      console.log(res.data.xml.prepay_id)
+      wx.requestPayment(
+        {
+          'timeStamp': '1490840662',
+          'nonceStr': '123',
+          'package': 'prepay_id=' + res.data.xml.prepay_id,
+          'signType': 'MD5',
+          'paySign': md5.hexMD5("appId=wxf0487d45228f02d3&nonceStr=123&package=prepay_id=" + res.data.xml.prepay_id + "&signType=MD5&timeStamp=1490840662&key=6e11af317a7a85a14b3387d5c6c71d3a").toUpperCase(),
+          'success': function (res) { console.log("success") },
+          'fail': function (res) { console.log("failed") },
+          'complete': function (res) { console.log("complete") }
+        })
+    }
+  })
+  },
   onLoad: function (options) {
     let that = this
     if (options.gameid) {
